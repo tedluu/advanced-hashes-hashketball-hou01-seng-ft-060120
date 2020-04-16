586edb42d8063d999a789bc46b935688c8630848
Thach Luu <luuphanbaothach@gmail.com>
2020-04-16 21:29:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,6 +128,7 @@ end
 def num_points_scored
   hash = game_hash
   game_hash.each do |location,info|
-    info.each do |attribute,|
+    info.each do |attribute,stuff|
+      
       
 # Write code here
