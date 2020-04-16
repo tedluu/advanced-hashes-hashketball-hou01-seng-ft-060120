348e7eefbfe46adafdf9190d91dd5371825cf024
Thach Luu <luuphanbaothach@gmail.com>
2020-04-16 22:28:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,11 +129,11 @@ end
 def num_points_scored(name)
   hash = game_hash
   hash.each do |location, info| 
-    info.each do |attribute, stuff| 
+    info[:players].each do |p| 
       
-     if stuff.include?(name)
+     if p[:player_name] == name 
           
-       return hash[location][attribute][points]
+       return p[:points] 
       end
     end
   end
